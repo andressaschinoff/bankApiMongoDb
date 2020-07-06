86b1253720f3c11f37fb8ac3f245d96a80aecce6
Andressa Schinoff <andressaschinoff@gmail.com>
2020-07-06 12:12:56 -0300

    debug mongodb connection

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ import { agencyRoute } from './routes/agencyRoute.js';
 
 const app = express();
 
+const host = process.env.DB_HOST;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
 const mongoDBConnection = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0-qasg0.mongodb.net/test?retryWrites=true&w=majority`,
+      'mongodb+srv://' +
+        username +
+        ':' +
+        password +
+        '@mongodb2020.qasg0.gcp.mongodb.net/bankIGTI?retryWrites=true&w=majority',
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
     console.log('DB connected');
@@ -25,4 +33,4 @@ app.use(express.json());
 app.use('/account', accountRoute);
 app.use('/agency', agencyRoute);
 
-app.listen(process.env.PORT || 3001, () => console.log('API started'));
+app.listen(host || 3001, () => console.log('API started'));
